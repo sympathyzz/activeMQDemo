367505281a4020ef4f012c0fed1082ec84fbb1a7
GAOGA4 <961977096@qq.com>
2020-08-09 22:43:14 +0800

    [Gavin] feature: activeMQDemo

---
+++ activeMQDemo/src/main/java/com/example/activeMQDemo/config/QueueConfig.java
@@ -0,0 +1,20 @@
+package com.example.activeMQDemo.config;
+
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.jms.Queue;
+
+
+@Configuration
+public class QueueConfig {
+    @Value("${queue}")
+    private String queue;
+
+    @Bean
+    public Queue logQueue(){
+        return new ActiveMQQueue(queue);
+    }
+}
